b45cdf3e75f606cb613bff95576873eac790e1d2
people92 <eminjae8659@gmail.com>
2021-08-23 09:42:51 +0900

    java example init commit

---
+++ java-study/src/test/java/com/study/example/java/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.example.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
