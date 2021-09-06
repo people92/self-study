a39b2bab82ce233a47fd94e06c1e91425a48b18d
people92 <eminjae8659@gmail.com>
2021-09-06 19:36:03 +0900

    feign retry added

---
+++ spring-cloud/src/main/java/com/study/springcloud/FeignConfiguration.java
@@ -0,0 +1,13 @@
+package com.study.springcloud;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, 2000, 3);
+    }
+}
